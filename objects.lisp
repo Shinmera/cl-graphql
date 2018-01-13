80288908ea25db3bfbab7e981e3096bac8b111d9
Shinmera <shinmera@tymoon.eu>
2018-01-13 18:18:12 +0100

    Add minor validation to representations.

--- objects.lisp
+++ objects.lisp
@@ -23,68 +23,102 @@
        vector       ; ListValue
        hash-table)) ; ObjectValue
 
-(defclass document ()
-  (definitions)) ; => definition*
+(defmacro define-representation (name direct-superclasses direct-slots &body options)
+  (let* ((slots (loop for slot in direct-slots
+                      collect (destructuring-bind (name &key (accessor name) (initarg (intern (string name) "KEYWORD")) type required &allow-other-keys) slot
+                                `(,name :accessor ,accessor :initarg ,initarg :type ,type :required ,required))))
+         (value (gensym "VALUE"))
+         (names (gensym "NAMES"))
+         (null (gensym "NULL")))
+    `(progn
+       (defclass ,name ,direct-superclasses
+         ,(loop for (slot . options) in slots
+                collect `(,slot :accessor ,(getf options :accessor) :initform NIL))
+         ,@options)
+
+       (defmethod initialize-instance :before ((,name ,name) &key ,@(loop for (slot . options) in slots
+                                                                          when (and (getf options :initarg) (getf options :required))
+                                                                          collect `(,(intern (string (getf options :initarg))) ',null)))
+         ,@(loop for (slot . options) in slots
+                 when (and (getf options :initarg) (getf options :required))
+                 collect `(when (eq ,(intern (string (getf options :initarg))) ',null)
+                            (error ,(format NIL "~a must be provided for ~as." (getf options :initarg) name)))))
+
+       (defmethod shared-initialize :after ((,name ,name) ,names &key ,@(loop for (slot . options) in slots
+                                                                              when (getf options :initarg)
+                                                                              collect (intern (string (getf options :initarg)))))
+         (declare (ignore ,names))
+         ,@(loop for (slot . options) in slots
+                 when (getf options :initarg)
+                 collect `(setf (,(getf options :accessor) ,name) ,(intern (string (getf options :initarg))))))
+
+       ,@(loop for (slot . options) in slots
+               when (getf options :type)
+               collect `(defmethod (setf ,(getf options :accessor)) :before (,value (,name ,name))
+                          (check-type ,value ,(getf options :type)))))))
+
+(define-representation document ()
+    ((definitions :type list)))
 
 ;;; Abstract
-(defclass definition ()
-  ())
+(define-representation definition ()
+    ())
 
 ;;; Abstract
-(defclass operation (definition)
-  (name  ; => name?
-   variable-definitions ; => variable-definition*?
-   directives ; => directive*?
-   selection-set)) ; => selection*
+(define-representation operation (definition)
+    ((name :type name :required T)
+     (variable-definitions :type list)
+     (directives :type list)
+     (selection-set :type list :required T)))
 
-(defclass query (operation)
-  ())
+(define-representation query (operation)
+    ())
 
-(defclass mutation (operation)
-  ())
-
-;;; Abstract
-(defclass selection ()
-  ())
-
-(defclass field (selection)
-  (alias ; => name?
-   name ; => name
-   arguments ; => (name -> value)?
-   directives ; => directive*?
-   selection-set)) ; => selection*?
-
-(defclass fragment-spread (selection)
-  (name ; => fragment-name
-   directives)) ; => directive*?
+(define-representation mutation (operation)
+    ())
 
 ;;; Abstract
-(defclass fragment ()
-  (type-condition ; => named-type?
-   directives ; => directive*?
-   selection-set)) ; => selection*
+(define-representation selection ()
+    ())
 
-(defclass inline-fragment (selection fragment)
-  ())
+(define-representation field (selection)
+    ((alias :type name)
+     (name :type name :required T)
+     (arguments :type list)
+     (directives :type list)
+     (selection-set :type list)))
 
-(defclass fragment-definition (definition fragment)
-  (name)) ; => fragment-name
-
-(defclass variable-definition (definition)
-  (name ; => name
-   type ; => graph-type
-   default-value)) ; => value?
+(define-representation fragment-spread (selection)
+    ((name :type fragment-name :required T)
+     (directives :type list)))
 
 ;;; Abstract
-(defclass graph-type ()
-  (not-null-p))
+(define-representation fragment ()
+    ((type-condition :type named-type)
+     (directives :type list)
+     (selection-set :type list :required T)))
 
-(defclass named-type (graph-type)
-  (name)) ; => name
+(define-representation inline-fragment (selection fragment)
+    ())
 
-(defclass list-type (graph-type)
-  (type)) ; => graph-type
+(define-representation fragment-definition (definition fragment)
+    ((name :type fragment-name :required T)))
 
-(defclass directive ()
-  (name
-   arguments)) ; (name -> value)?
+(define-representation variable-definition (definition)
+    ((name :type name :required T)
+     (value-type :type graph-type :required T)
+     (default-value :type value)))
+
+;;; Abstract
+(define-representation graph-type ()
+    ((not-null-p)))
+
+(define-representation named-type (graph-type)
+    ((name :type name :required T)))
+
+(define-representation list-type (graph-type)
+    ((item-type :type graph-type :required T)))
+
+(define-representation directive ()
+    ((name :type name :required T)
+     (arguments :type list)))
